3b8d083a69298db4aea2fc8753ee0871c94a8254
Jairopln <87547016+jairomatheus89@users.noreply.github.com>
2026-03-10 17:30:21 -0300

    Add project files.

---
+++ pim3semestre/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pim3semestre.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
