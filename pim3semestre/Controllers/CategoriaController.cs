11a04121e76a92eeac7aad82ed4527e1446a2ce8
kishinbr <viniciuskishibr@gmail.com>
2026-03-19 15:20:30 -0300

    Criando a Categoria
    crud completo da categoria

---
+++ pim3semestre/Controllers/CategoriaController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pim3semestre.Data;
+using pim3semestre.Models;
+
+namespace pim3semestre.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        //atibuto para acessar o banco de dados , por leitura somente
+        readonly ApplicationDbContext _db;
+
+        //construtor para injetar o banco de dados
+        public CategoriaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        //acao default quando acessar a rota /Categoria , listando as categorias disponiveis
+        public IActionResult Index()
+        {
+            IEnumerable<CategoriaModel> categorias = _db.Categorias;
+
+            return View(categorias);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CategoriaModel? categoria = _db.Categorias.FirstOrDefault(x => x.CategoriaID == id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoria);
+        }
+
+        [HttpPost]
+
+        public IActionResult Editar(CategoriaModel categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categorias.Update(categoria);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            CategoriaModel? categoria = _db.Categorias.FirstOrDefault(x => x.CategoriaID == id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(CategoriaModel categoria)
+        {
+            if(categoria == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categorias.Remove(categoria);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public IActionResult Cadastrar(CategoriaModel categoria)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categorias.Add(categoria);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
--- pim3semestre/Models/CategoriaModel.cs
+++ pim3semestre/Models/CategoriaModel.cs
@@ -7,8 +7,11 @@ namespace pim3semestre.Models
         [Key]
         public int CategoriaID { get; set; }
 
+
+        [Required(ErrorMessage = "O nome da categoria é obrigatório.")]
         public string? CategoriaNome { get; set; }
 
+        [Required(ErrorMessage = "A descrição da categoria é obrigatória.")]
         public string? CategoriaDescricao { get; set; }
 
         public List<ProdutoModel> Produtos { get; set; } = new List<ProdutoModel>();
